bc9d2b35b24d4234cd38e06378dffa7a7ae4415c
mambaye <455792335@qq.com>
2016-12-21 17:04:34 +0800

    add constant

--- app/src/main/java/com/zhanghao/skinexpert/fragments/BenefitsFragment.java
+++ app/src/main/java/com/zhanghao/skinexpert/fragments/BenefitsFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -19,20 +21,21 @@ public class BenefitsFragment extends Fragment {
     private View view;
     private WebView webView;
     private String benefitsURL = "http://www.caimiapp.com/fllbas/?token=&skin=----&source=app";
+    private WebSettings settings;
     public BenefitsFragment() {
 
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         if (view == null) {
             view = inflater.inflate(R.layout.fragment_benefits, container, false);
             webView = (WebView) view.findViewById(R.id.webview_benefits);
-            webView.getSettings().setJavaScriptEnabled(true);
+           settings = webView.getSettings();
+            settings.setJavaScriptEnabled(true);
             webView.setWebViewClient(new WebViewClient());
+            webView.setWebChromeClient(new WebChromeClient());
             webView.loadUrl(benefitsURL);
-
         }
         return view;
     }
---
+++ app/src/main/java/com/zhanghao/skinexpert/utils/NetWorkRequest.java
@@ -0,0 +1,92 @@
+package com.zhanghao.skinexpert.utils;
+
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by 黑曼巴ye on 2016/12/21.
+ */
+
+public class NetWorkRequest {
+    private static RequestQueue requestQueue;
+
+    public static void postMainData(Context context, String path, final String token, final String total, final RequestCallBack callBack) {
+        requestQueue = Volley.newRequestQueue(context);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, path, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                callBack.success(s);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                callBack.fail("访问有误");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+                map.put("token", token);
+                map.put("total", total);
+                return map;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+    public static void postDetailData(Context context, final String path, final String fid, final String bid, final String cid, final String price_id, final String eid,
+                                      final String selectType, final String total, final String skinCode, final String token, final RequestCallBack callBack) {
+
+        requestQueue = Volley.newRequestQueue(context);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, path, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                callBack.success(s);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                callBack.fail("访问有误");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+                map.put("fid", fid);
+                map.put("bid", bid);
+                map.put("cid", cid);
+                map.put("price_id", price_id);
+                map.put("eid", eid);
+                map.put("selectType", selectType);
+                map.put("total", total);
+                map.put("skinCode", skinCode);
+                map.put("token", token);
+                return map;
+            }
+        };
+        requestQueue.add(stringRequest);
+
+    }
+
+    /*
+     通过此接口与用户可在需要访问网络的地方获取结果
+      */
+    public interface RequestCallBack {
+        void success(String result);
+
+        void fail(String result);
+    }
+
+}
