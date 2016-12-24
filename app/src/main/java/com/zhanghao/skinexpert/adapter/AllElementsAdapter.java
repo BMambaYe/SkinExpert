81885bd569a45d76e705c9a712bd5f2d08faa763
mambaye <455792335@qq.com>
2016-12-24 20:33:38 +0800

    12312312312

---
+++ app/src/main/java/com/zhanghao/skinexpert/adapter/AllElementsAdapter.java
@@ -0,0 +1,10 @@
+package com.zhanghao.skinexpert.adapter;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 黑曼巴ye on 2016/12/24.
+ */
+
+public class AllElementsAdapter extends BaseAdapter {
+}
---
+++ app/src/main/java/com/zhanghao/skinexpert/beans/ElementDetailBean.java
@@ -0,0 +1,8 @@
+package com.zhanghao.skinexpert.beans;
+
+/**
+ * Created by 黑曼巴ye on 2016/12/24.
+ */
+
+public class ElementDetailBean {
+}
