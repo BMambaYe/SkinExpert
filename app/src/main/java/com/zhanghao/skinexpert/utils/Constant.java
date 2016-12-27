d0bfc50abc646b94c3ffc82e337ae5491e5bd8e8
mambaye <455792335@qq.com>
2016-12-27 13:15:57 +0800

    update wanted bug

--- app/src/main/java/com/zhanghao/skinexpert/utils/Constant.java
+++ app/src/main/java/com/zhanghao/skinexpert/utils/Constant.java
@@ -46,8 +46,6 @@ public class Constant {
     public static final String DETAILALLDISGUSS3="&isFirst=1";
     public static final String EXPERTSUGGESTION="http://www.caimiapp.com/cpzjjyu/?token=&id=";
     public static final String EXPERTSUGGESTION1="&skin=";
-
-
     public static final String SKINT_TEST_RESULT_URL="http://www.caimiapp.com/syjftda_c/";
     public static final String SKINT_TEST_DESCRIPTION="http://www.caimiapp.com/sy16_c/";
     public static final String SKIN_TEST_RESULT_DB ="skintestresult";
